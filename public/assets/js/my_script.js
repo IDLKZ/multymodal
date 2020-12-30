cc3d51a6504f9c8a70c2393b1ab8a56580ba3397
IDLKZ <idlkz2019@gmail.com>
2020-12-30 11:49:03 +0600

    Commit

---
+++ public/assets/js/my_script.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el:'.swiper-pagination',
+        clickable: true,
+    },
+})
+var swiper2 = new Swiper('.swiper-container2', {
+    direction: 'vertical',
+    slidesPerView: 1,
+    mousewheel: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+swiper2.on('slideChange', function () {
+    swiper.slideTo(swiper2.activeIndex)
+});
+swiper.on('slideChange', function () {
+    swiper2.slideTo(swiper.activeIndex)
+});
+var swiper3 = new Swiper('.swiper-container3', {
+    loop:true,
+    spaceBetween: 50,
+    autoplay:true,
+    slidesPerView: window.screen.width >900 ? 3 : 1,
+    mousewheel: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+});
