435cb5c3d961888e005a6d77e6f658af50501482
IDLKZ <idlkz2019@gmail.com>
2020-12-30 12:30:24 +0600

    Commit

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -14,6 +14,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset("/assets/css/mystyle.css")}}">
 
 
