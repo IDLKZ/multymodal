39895a4e777e7bd23bba2f0ddfd59ae50842d7d5
IDLKZ <idlkz2019@gmail.com>
2020-12-30 12:36:18 +0600

    Commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -354,7 +354,7 @@
                     <div class="col-md-6">
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
-                            <label class="form-check-label" for="defaultCheck1">
+                            <label class="form-check-label text-white" for="defaultCheck1">
                                 Я принимаю условия конфиденциальности
                             </label>
                         </div>
